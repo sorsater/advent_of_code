bfa1f385bc11c0e28eb387ed87a31762d155d77b
michael <michael.sorsater@gmail.com>
2017-12-17 16:50:27 +0100

    Adding progressbar :watch:

--- 2017/17.py
+++ 2017/17.py
@@ -3,23 +3,26 @@
 
 step_count = 301
 
+import sys
+sys.path.append('..')
+from w8m8 import progressbar
+
 cur_idx = 0
 code = [0]
-for i in range(1, 2017+1):
-    cur_idx = (cur_idx + step_count) % len(code)
-    code.insert(cur_idx+1, i)
-    cur_idx += 1
+for i in range(2017):
+    cur_idx = (cur_idx + step_count) % (i+1) + 1
+    code.insert(cur_idx, i+1)
 
 print('Answer #1: {}'.format(code[code.index(2017)+1]))
 
+# Zero is always at the beginning of the code
+# Save value if it have index 1
 cur_idx = 0
 best = -1
-# Zero is always at the beginning
-# Save just the value if it is after 0
-for i in range(1, 50000000+1):
-    cur_idx = (cur_idx + step_count) % i
-    if cur_idx == 0:
-        best = i
-    cur_idx += 1
-print('Answer #2: {}'.format(best))
+n_iter = 50000000
+for i in range(n_iter):
+    cur_idx = (cur_idx + step_count) % (i+1) + 1
+    best = i + 1 if cur_idx == 1 else best
+    progressbar(i/n_iter) if i % 100000 == 0 else None
+print('\033[KAnswer #2: {}'.format(best))
 
